8a4c48732b131f43b9c05430e64f334fc2b259fc
Hannah.Celemen <hcelemen@icloud.com>
2022-11-22 16:45:47 -0700

    Remove comments

--- src/App.js
+++ src/App.js
@@ -2,28 +2,35 @@ import React, { useEffect, useReducer } from 'react';
 import Ideas from './Ideas';
 import Form from './Form';
 import AppContext from './AppContext';
-// import ThemeContext from './ThemeContext';
 import './App.css';
 
-// useReducer and dispatch
 const initialState = {
   theme: 'light',
-  ideas: [],
+  ideas: [
+    {
+      id: 1,
+      title: 'Flying ferret',
+      description: 'butterfly wings',
+    },
+    {
+      id: 2,
+      title: 'self-control',
+      description: 'delay gratification for long term success',
+    },
+  ],
 };
 
 const reducer = (state, action) => {
-  console.log(action);
+  console.log(action, 'action App line 24');
   switch (action.type) {
     case 'TOGGLE_THEME':
       const newTheme = state.theme === 'light' ? 'dark' : 'light';
       return { ...state, theme: newTheme };
 
     case 'ADD_IDEAS':
-      console.log(action.idea);
       return { ...state, ideas: [...state.ideas, action.idea] };
 
     case 'REMOVE_IDEAS':
-      console.log(action.id);
       const filteredIdeas = state.ideas.filter((idea) => idea.id !== action.id);
       return { ...state, ideas: filteredIdeas };
 
@@ -33,36 +40,18 @@ const reducer = (state, action) => {
 };
 
 function App() {
-  // const [ideas, setIdeas] = useState([]);
-  // const [theme, setTheme] = useState('light');
   const [state, dispatch] = useReducer(reducer, initialState);
-  console.log(state, '=', dispatch);
+  console.log(state.ideas.length);
   useEffect(() => {
     document.title = `Ideabox (${state.ideas.length})`;
   });
 
-  // const addIdea = (newIdea) => {
-  //   // logic was moved into reducer
-  //   const action = { type: 'ADD_IDEAS', idea: newIdea };
-  //   dispatch(action);
-  // };
-
-  // const deleteIdea = (id) => {
-  //   // logic was moved into reducer
-  //   const action = { type: 'REMOVE_IDEAS', id: id };
-  //   dispatch(action);
-  // };
-
   const toggleTheme = () => {
-    // const newTheme = theme === 'light' ? 'dark' : 'light';
-    // setTheme(newTheme);
-    // this logic has been moved into our reducer variable so now we use dispatch to invoke change
     const action = { type: 'TOGGLE_THEME' };
     dispatch(action);
   };
 
   return (
-    // <ThemeContext.Provider value={state.theme}>
     <AppContext.Provider value={[state, dispatch]}>
       <main className="App">
         <h1>IdeaBox</h1>
@@ -71,7 +60,6 @@ function App() {
         <Ideas />
       </main>
     </AppContext.Provider>
-    // </ThemeContext.Provider>
   );
 }
 
--- src/AppContext.js
+++ src/AppContext.js
@@ -3,5 +3,3 @@ import { createContext } from 'react';
 const AppContext = createContext();
 
 export default AppContext;
-
-// we will need to import this wherever we are providing or consuming context i.e. App.js
