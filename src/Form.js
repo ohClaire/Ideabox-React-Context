c6cdaacdf6af1ddee88090f90464fda2a8a61998
Hannah.Celemen <hcelemen@icloud.com>
2022-11-22 14:32:31 -0700

    Change Form

--- src/Form.js
+++ src/Form.js
@@ -1,47 +1,49 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import AppContext from './AppContext';
 import './Form.css';
 
 function Form(props) {
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
+  // const [title, setTitle] = useState('')
+  // const [description, setDescription] = useState('')
+  const [state, dispatch] = useContext(AppContext);
 
-  const submitIdea = event => {
+  const submitIdea = (event) => {
     event.preventDefault();
     const newIdea = {
       id: Date.now(),
       title,
-      description
-    }
-    props.addIdea(newIdea);
+      description,
+    };
+    dispatch({ type: 'ADD_IDEAS', idea: newIdea });
     clearInputs();
-  }
+  };
 
   const clearInputs = () => {
-    setTitle('')
-    setDescription('')
-  }
+    setTitle('');
+    setDescription('');
+  };
 
   return (
     <form>
       <input
-        type='text'
-        placeholder='Title'
-        name='title'
+        type="text"
+        placeholder="Title"
+        name="title"
         value={title}
-        onChange={event => setTitle(event.target.value)}
+        onChange={(event) => setTitle(event.target.value)}
       />
 
       <input
-        type='text'
-        placeholder='Description'
-        name='description'
+        type="text"
+        placeholder="Description"
+        name="description"
         value={description}
-        onChange={event => setDescription(event.target.value)}
+        onChange={(event) => setDescription(event.target.value)}
       />
 
       <button onClick={submitIdea}>SUBMIT</button>
     </form>
-  )
+  );
 }
 
 export default Form;
--- src/Ideas.js
+++ src/Ideas.js
@@ -4,23 +4,17 @@ import Card from './Card';
 import './Ideas.css';
 
 const Ideas = () => {
-  const [ideas] = useContext(AppContext);
-  console.log(ideas);
+  const [state, dispatch] = useContext(AppContext);
+  console.log(state);
 
-  const deleteIdea = (id) => {
-    // logic was moved into reducer
-    const action = { type: 'REMOVE_IDEAS', id: id };
-    dispatch(action);
-  };
-
-  const ideaCards = ideas.map((idea) => {
+  const ideaCards = state.ideas.map((idea) => {
     return (
       <Card
         title={idea.title}
         description={idea.description}
         id={idea.id}
         key={idea.id}
-        deleteIdea={() => deleteIdea(idea.id)}
+        // deleteIdea={() => deleteIdea(idea.id)}
       />
     );
   });
